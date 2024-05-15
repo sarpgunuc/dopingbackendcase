5bcb5ab767e7924906bc66035ecae887031985d3
sarpgunuc <115190060+sarpgunuc@users.noreply.github.com>
2024-05-15 16:06:56 +0300

    I am starting to project

---
+++ dopingchal/src/main/java/com/doping/dopingchal/DopingchalApplication.java
@@ -0,0 +1,15 @@
+package com.doping.dopingchal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DopingchalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DopingchalApplication.class, args);
+		
+		System.out.println("projeye başlıyoruz !!");
+	}
+
+}
---
+++ dopingchal/src/test/java/com/doping/dopingchal/DopingchalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doping.dopingchal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DopingchalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
